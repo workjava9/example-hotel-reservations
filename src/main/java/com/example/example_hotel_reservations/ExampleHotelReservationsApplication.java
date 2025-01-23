0b8a849eae779dc1b9a6d412164ec1805bf1a6c6
d3mka <pasakirpicev6377@gmail.com>
2025-01-23 10:59:30 +0300

    Initial commit

---
+++ src/main/java/com/example/example_hotel_reservations/ExampleHotelReservationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.example_hotel_reservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleHotelReservationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleHotelReservationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/example_hotel_reservations/ExampleHotelReservationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.example_hotel_reservations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleHotelReservationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
